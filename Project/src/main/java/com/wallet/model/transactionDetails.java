7d38f64d8c7e7278211aca355d922b4255d56b74
Ramanpreet Kaur Kohli <51257414+KohliRamanpreet@users.noreply.github.com>
2020-04-27 22:58:19 +0530

    Update transactionDetails.java

--- Project/src/main/java/com/wallet/model/transactionDetails.java
+++ Project/src/main/java/com/wallet/model/transactionDetails.java
@@ -23,12 +23,6 @@ public class transactionDetails {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name="transaction_id")
 	private int transactionId;
-	/*
-	 * @ManyToOne(cascade =CascadeType.ALL )
-	 * 
-	 * @JoinColumn(name="User_name",referencedColumnName = "userName") private
-	 * userDetails user;
-	 */
 	@Column(name="account_id")
 	private long accountId;
 	@Column(name="sourceAcc")
@@ -44,32 +38,27 @@ public class transactionDetails {
 	private Date timeStamp;
 	@Column(name="amtTransfered")
 	private double amtTransfered;
-public int getTransactionId() {
+        public int getTransactionId() {
 		return transactionId;
 	}
 
-public String getType() {
-	return type;
-}
-public void setType(String type) {
-	this.type = type;
-}
+       public String getType() {
+	         return type;
+       }
+       public void setType(String type) {
+	      this.type = type;
+             }
 	public void setTransactionId(int transactionId) {
 		this.transactionId = transactionId;
 	}
-	//	public userDetails getUser() {
-//		return user;
-//	}
-//	public void setUser(userDetails user) {
-//		this.user = user;
-//	}
+	
+
 	public long getAccountId() {
 		return accountId;
 	}
 	public void setAccountId(long accountId) {
 		this.accountId = accountId;
-	}
-//	
+		
 	public long getSourceAcc() {
 		return sourceAcc;
 	}
