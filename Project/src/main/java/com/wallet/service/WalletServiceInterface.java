c6c7964c163adffb56c28d48c83274564f2766b4
Ramanpreet Kaur Kohli <51257414+KohliRamanpreet@users.noreply.github.com>
2020-04-27 23:17:29 +0530

    Update WalletServiceInterface.java

--- Project/src/main/java/com/wallet/service/WalletServiceInterface.java
+++ Project/src/main/java/com/wallet/service/WalletServiceInterface.java
@@ -1,30 +1,19 @@
 package com.wallet.service;
-
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import com.wallet.model.transactionDetails;
 import com.wallet.model.userDetails;
-
 @Service
 public interface WalletServiceInterface {
 	userDetails createAccount(userDetails user);
 	List<userDetails> findAllAccount();
-	byte[] passwordDecryptor(byte[] password);
-	byte[] passwordEncryptor(byte[] password);
 	userDetails findAccountById(int accountId);
 	List<transactionDetails> printTransactions(int accountId);
 	boolean fundTransfer(int senderId, int receiverId, float money);
 	userDetails updateAccount(userDetails customer);
 	boolean withdraw(int accountId, float money);
 	boolean deposit(int accountId, float money);
-
-	
-	boolean depositF(int accountId, float money, int sourceAcc);
-	boolean withdrawF(int accountId, float money, int senderId);
 	boolean deleteAccountById(int accountID);
-	userDetails updateAccount1(userDetails customer);
 	boolean fundTransfer1(int senderId, int receiverId, float money);
 
 }
